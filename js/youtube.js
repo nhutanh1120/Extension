1e353a2389ae6b913e4a132ae1af6b5c7b0eb6e3
nhutanh1120 <73190944+nhutanh1120@users.noreply.github.com>
2022-11-26 00:39:05 +0700

    Optimize website performance

--- js/youtube.js
+++ js/youtube.js
@@ -1,5 +1,30 @@
-setInterval(function(){
-    if(document.querySelector(".ytp-ad-skip-button.ytp-button")) {
-        document.querySelector(".ytp-ad-skip-button.ytp-button").click()
-    }
-}, 300)
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if (mutation.addedNodes.length) {
+            if (document.querySelector(".ytp-ad-skip-button.ytp-button")) {
+                document.querySelector(".ytp-ad-skip-button.ytp-button").click();
+            }
+            if (
+                document.querySelector(
+                    "#container .ytp-ad-overlay-container.ytp-rounded-overlay-ad"
+                )
+            ) {
+                document
+                    .querySelector(
+                        "#container .ytp-ad-overlay-container.ytp-rounded-overlay-ad"
+                    )
+                    .remove();
+            }
+        }
+    })
+})
+
+var domElement = document.querySelector("body")
+observer.observe(domElement, {
+    childList: true,
+    characterData: true,
+    atributes: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+})
