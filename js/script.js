b69dc2e99e4f7f8cb377de7a803242f41a6c8a71
nhutanh1120 <73190944+nhutanh1120@users.noreply.github.com>
2022-12-25 16:06:38 +0700

    Update theme switch for extension

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const tabsElements = document.querySelectorAll('.tab-list .tab-item');
 const panesElements = document.querySelectorAll('.tab-content .tab-pane');
 
-const tabActive = document.querySelector('.tabs .tab-item.active')
+const tabActive = document.querySelector('.tabs .tab-item.active');
 const line = document.querySelector('.tabs .line');
+
+const toggleElements = document.querySelectorAll('.toggle');
 
 line.style.width = tabActive.offsetWidth + 'px';
 line.style.left = tabActive.offsetLeft + 'px';
@@ -18,5 +20,18 @@ tabsElements.forEach((tab, index) => {
 
         tab?.classList.add('active');
         pane?.classList.add('active');
-    }
-}); 
+    };
+});
+
+toggleElements.forEach(toggle => {
+    toggle.onclick = function () {
+        this?.firstElementChild.classList.toggle('active');
+        if (this.id === 'toggle-theme') {
+            if (this?.firstElementChild.classList[0] === 'active' || this?.firstElementChild.classList[1] === 'active') {
+                document.documentElement.setAttribute('data-theme', 'dark');
+            } else {
+                document.documentElement.setAttribute('data-theme', 'light');
+            };
+        };
+    };
+});
