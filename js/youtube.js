5b0912ccecfef8610d5bdf4413aee28d464ae04d
nhutanh1120 <nhutanh1120@gmail.com>
2023-11-24 00:43:08 +0700

    20231124

--- js/youtube.js
+++ js/youtube.js
@@ -1,28 +1,41 @@
 var observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
         if (mutation.addedNodes.length) {
-            if (document.querySelector(".ytp-ad-skip-button.ytp-button")) {
-                document.querySelector(".ytp-ad-skip-button.ytp-button").click();
+
+            const container = document.querySelector(".video-ads");
+            if (!container || container?.childElementCount === 0) {
+                return true;
             }
-            if (
-                document.querySelector(
-                    "#container .ytp-ad-overlay-container.ytp-rounded-overlay-ad"
-                )
-            ) {
-                document
-                    .querySelector(
-                        "#container .ytp-ad-overlay-container.ytp-rounded-overlay-ad"
-                    )
-                    .remove();
+            const ytpAdSkipButton = document.querySelector(".ytp-ad-skip-button.ytp-button");
+            if (ytpAdSkipButton) {
+                ytpAdSkipButton.click();
+                return true;
             }
-            if(document.getElementById("confirm-button")) {
-                document.getElementById("confirm-button").click();
+            const ytpAdSkipButtonModern = document.querySelector(".ytp-ad-skip-button-modern.ytp-button");
+            if (ytpAdSkipButtonModern) {
+                ytpAdSkipButtonModern.click();
+                return true;
+            }
+            const ytpAdOverlayContainer = document.querySelector("#container .ytp-ad-overlay-container.ytp-rounded-overlay-ad");
+            if (ytpAdOverlayContainer) {
+                ytpAdOverlayContainer.remove();
+                return true;
+            }
+            const confirmButton = document.getElementById("confirm-button");
+            if (confirmButton) {
+                confirmButton.click();
+                return true;
+            }
+            const ytpAutonavEndscreenUpnextPlayButton = document.querySelector(".ytp-autonav-endscreen-button-container .ytp-autonav-endscreen-upnext-cancel-button~.ytp-autonav-endscreen-upnext-play-button");
+            if (ytpAutonavEndscreenUpnextPlayButton) {
+                ytpAutonavEndscreenUpnextPlayButton.click();
+                return true;
             }
         }
     })
 })
 
-var domElement = document.querySelector("body")
+var domElement = document.querySelector("body");
 observer.observe(domElement, {
     childList: true,
     characterData: true,
@@ -30,4 +43,4 @@ observer.observe(domElement, {
     subtree: true,
     attributeOldValue: true,
     characterDataOldValue: true
-})
+});
