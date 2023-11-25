2540aa82b3dbc62e0e2de785619dd27433c496f2
nhutanh1120 <nhutanh1120@gmail.com>
2023-11-26 03:34:54 +0700

    20231126

--- js/script.js
+++ js/script.js
@@ -1,37 +1,113 @@
-const tabsElements = document.querySelectorAll('.tab-list .tab-item');
-const panesElements = document.querySelectorAll('.tab-content .tab-pane');
+// Hàm kiểm tra xem một URL có phải là trang YouTube không
+function isYouTubeTab(url) {
+    const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/(c\/.+\/)?|youtu\.be\/)/;
+    return youtubeRegex.test(url);
+}
 
-const tabActive = document.querySelector('.tabs .tab-item.active');
-const line = document.querySelector('.tabs .line');
+// Hàm gửi thông điệp đến tab YouTube với dữ liệu cần thiết
+function sendMessageToYouTubeTab(tabId, dataToSend) {
+    chrome.tabs.sendMessage(tabId, { message: "sendData", data: dataToSend });
+}
 
-const toggleElements = document.querySelectorAll('.toggle');
+// Hàm thiết lập tab và pane làm tab và pane hiện tại
+function setActiveTab(tab, pane) {
+    const activeTab = document.querySelector(".tabs .tab-item.active");
+    const activePane = document.querySelector(".tabs .tab-pane.active");
 
-line.style.width = tabActive.offsetWidth + 'px';
-line.style.left = tabActive.offsetLeft + 'px';
+    line.style.width = `${tab.offsetWidth}px`;
+    line.style.left = `${tab.offsetLeft}px`;
 
+    activeTab?.classList.remove("active");
+    activePane?.classList.remove("active");
+
+    tab.classList.add("active");
+    pane.classList.add("active");
+}
+
+// Hàm xử lý sự kiện khi toggle theme hoặc toggle YouTube
+function toggleTheme() {
+    const toggle = this;
+    const toggleIcon = toggle?.firstElementChild;
+
+    if (!toggleIcon) return;
+
+    toggleIcon.classList.toggle("active");
+
+    if (toggle.id === "toggle-theme") {
+        setTheme(toggleIcon.classList.contains("active"));
+    }
+    if (toggle.id === "toggle-youtube") {
+        toggleYouTube(toggleIcon.classList.contains("active"));
+    }
+}
+
+// Hàm thiết lập chủ đề cho trang web
+function setTheme(isDark) {
+    const theme = isDark ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+}
+
+// Hàm xử lý sự kiện khi toggle YouTube
+function toggleYouTube(dataToSend) {
+    localStorage.setItem("observe", dataToSend);
+    chrome.tabs.query({}, function (tabs) {
+        if (tabs && tabs.length > 0) {
+            for (let i = 0; i < tabs.length; i++) {
+                const currentTab = tabs[i];
+                const tabURL = currentTab.url;
+
+                if (isYouTubeTab(tabURL)) {
+                    sendMessageToYouTubeTab(currentTab.id, dataToSend);
+                }
+            }
+        }
+    });
+}
+
+// Hàm khởi tạo trạng thái của toggle theme khi trang web được load
+function initializeThemeToggle() {
+    const themeToggleElement = document.querySelector("#toggle-theme");
+    const storedTheme = localStorage.getItem("theme");
+    const toggleIcon = themeToggleElement.firstElementChild;
+
+    if (storedTheme === "dark") {
+        toggleIcon.classList.add("active");
+    } else {
+        toggleIcon.classList.remove("active");
+    }
+
+    // Gọi hàm setTheme với trạng thái hiện tại của toggle
+    setTheme(toggleIcon.classList.contains("active"));
+}
+
+// Hàm khởi tạo trạng thái của toggle YouTube khi trang web được load
+function initializeObserverToggle() {
+    const observeToggleElement = document.querySelector("#toggle-youtube");
+    const observe = localStorage.getItem("observe");
+    if (observe && observe !== "false") {
+        observeToggleElement.firstElementChild.classList.add("active");
+        localStorage.setItem("observe", true);
+    }
+}
+
+// Lấy tất cả các tab và pane
+const tabsElements = document.querySelectorAll(".tab-list .tab-item");
+const panesElements = document.querySelectorAll(".tab-content .tab-pane");
+const toggleElements = document.querySelectorAll(".toggle");
+const line = document.querySelector(".tabs .line");
+
+// Gắn sự kiện click cho mỗi tab
 tabsElements.forEach((tab, index) => {
     const pane = panesElements[index];
-    tab.onclick = () => {
-        document.querySelector('.tabs .tab-item.active')?.classList.remove('active');
-        document.querySelector('.tabs .tab-pane.active')?.classList.remove('active');
-
-        line.style.width = tab.offsetWidth + 'px';
-        line.style.left = tab.offsetLeft + 'px';
-
-        tab?.classList.add('active');
-        pane?.classList.add('active');
-    };
+    tab.addEventListener("click", () => setActiveTab(tab, pane));
 });
 
-toggleElements.forEach(toggle => {
-    toggle.onclick = function () {
-        this?.firstElementChild.classList.toggle('active');
-        if (this.id === 'toggle-theme') {
-            if (this?.firstElementChild.classList[0] === 'active' || this?.firstElementChild.classList[1] === 'active') {
-                document.documentElement.setAttribute('data-theme', 'dark');
-            } else {
-                document.documentElement.setAttribute('data-theme', 'light');
-            };
-        };
-    };
+// Gắn sự kiện click cho mỗi toggle
+toggleElements.forEach((toggle) => {
+    toggle.addEventListener("click", toggleTheme);
 });
+
+// Khởi tạo trạng thái của toggle theme và toggle YouTube khi trang web được load
+initializeThemeToggle();
+initializeObserverToggle();
--- js/youtube.js
+++ js/youtube.js
@@ -1,42 +1,94 @@
-var observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-        const container = document.querySelector(".video-ads");
-        if (!container || container.childElementCount === 0) return;
+// Biến toàn cục để lưu trữ đối tượng MutationObserver
+let observer;
 
-        const selectors = [".ytp-ad-skip-button.ytp-button", ".ytp-ad-skip-button-modern.ytp-button"];
-        for (const selector of selectors) {
-            const button = document.querySelector(selector);
-            if (button) {
-                button.click();
-                return true;
-            }
-        }
+// Mảng chứa các selector để tìm kiếm các phần tử quảng cáo trong DOM
+const selectors = [
+    ".ytp-ad-skip-button.ytp-button",
+    ".ytp-ad-skip-button-modern.ytp-button",
+    "#container .ytp-ad-overlay-container.ytp-rounded-overlay-ad",
+    ".ytp-autonav-endscreen-button-container .ytp-autonav-endscreen-upnext-cancel-button~.ytp-autonav-endscreen-upnext-play-button",
+];
 
-        const overlayContainer = document.querySelector("#container .ytp-ad-overlay-container.ytp-rounded-overlay-ad");
-        if (overlayContainer) {
-            overlayContainer.remove();
-            return true;
-        }
+// Hàm xử lý hành động cho từng phần tử quảng cáo dựa trên selector
+function handleElementAction(selector, element) {
+    if (selector.includes("ytp-ad-skip-button") || selector.includes("ytp-autonav-endscreen-upnext-play-button")) {
+        element.click();
+    } else if (selector.includes("ytp-ad-overlay-container")) {
+        element.remove();
+    }
+}
 
-        const confirmButton = document.getElementById("confirm-button");
-        if (confirmButton) {
-            confirmButton.click();
-            return true;
-        }
+// Hàm xử lý sự kiện mutation, kiểm tra và xử lý các thay đổi trong DOM
+function handleMutation(mutations) {
+    // Kiểm tra và ấn nút xác nhận nếu tồn tại
+    const confirmButton = document.querySelector("#confirm-button");
+    if (confirmButton) {
+        confirmButton.click();
+        return;
+    }
 
-        const playButton = document.querySelector(".ytp-autonav-endscreen-button-container .ytp-autonav-endscreen-upnext-cancel-button~.ytp-autonav-endscreen-upnext-play-button");
-        if (playButton) {
-            playButton.click();
-            return true;
-        }
+    // Kiểm tra và xử lý các phần tử quảng cáo trong container video
+    const container = document.querySelector(".video-ads");
+    if (!container || container.childElementCount === 0) {
+        return;
+    }
+
+    // Duyệt qua mảng selector và xử lý từng phần tử
+    selectors.forEach((selector) => {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach((element) => {
+            handleElementAction(selector, element);
+        });
     });
-});
+}
 
-observer.observe(document.querySelector("body"), {
-    childList: true,
-    characterData: true,
-    attributes: true,
-    subtree: true,
-    attributeOldValue: true,
-    characterDataOldValue: true
-});
+// Hàm thêm MutationObserver để theo dõi các thay đổi trong DOM
+function addObserver() {
+    if (!observer) {
+        observer = new MutationObserver(handleMutation);
+        const config = {
+            childList: true,
+            characterData: true,
+            attributes: true,
+            subtree: true,
+            attributeOldValue: true,
+            characterDataOldValue: true,
+        };
+        observer.observe(document.body, config);
+    }
+}
+
+// Hàm ngừng theo dõi các thay đổi trong DOM
+function removeObserver() {
+    if (observer) {
+        observer.disconnect();
+        observer = null;
+    }
+}
+
+// Hàm kiểm tra và khởi tạo quan sát nếu cần
+function initializeObserver() {
+    const observe = localStorage.getItem("observe");
+    if (!observe || observe === "true") {
+        addObserver();
+    }
+}
+
+// Hàm xử lý thông điệp từ extension popup
+function handleMessage(request) {
+    if (request.message === "sendData") {
+        const observeValue = request.data;
+        if (observeValue) {
+            addObserver();
+        } else {
+            removeObserver();
+        }
+        localStorage.setItem("observe", observeValue);
+    }
+}
+
+// Lắng nghe sự kiện khi có thông điệp từ extension popup
+chrome.runtime.onMessage.addListener(handleMessage);
+
+// Khởi tạo quan sát khi trang web được load
+initializeObserver();
